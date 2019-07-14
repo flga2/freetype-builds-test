e290b253f64fec4c5ab94ffff2f98493addcb225
flga <flga@users.noreply.github.com>
2019-07-14 14:33:54 +0100

    travis

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+// #cgo CFLAGS: -I${SRCDIR}/dist/linux/include -I${SRCDIR}/dist/linux/include/freetype2 -Werror -Wall -Wextra -Wno-unused-parameter
+// #cgo LDFLAGS: -L${SRCDIR}/dist/linux/lib -lfreetype -lm
+import "C"
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var ft C.FT_Library
+	if err := C.FT_Init_FreeType(&ft); err != 0 {
+		os.Exit(err)
+	}
+	var amajor, aminor, apatch C.FT_Int
+	C.FT_Library_Version(l.ptr, &amajor, &aminor, &apatch)
+	fmt.Printf("%d.%d.%d", amajor, aminor, apatch)
+}
